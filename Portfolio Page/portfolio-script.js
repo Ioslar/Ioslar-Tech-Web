6170a6b8ffa6b2407b1297e219b489e7b2cff4ea
Ioslar <101954529+Ioslar@users.noreply.github.com>
2022-07-13 22:43:08 -0300

    Add files via upload

---
+++ Portfolio Page/portfolio-script.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const web1 = document.querySelector(".web-1");
+const web2 = document.querySelector(".web-2");
+const web3 = document.querySelector(".web-3");
+
+web1.addEventListener("click", ()=>{
+    window.location.href = "https://www.academiashopping.com.uy";
+})
+
+web2.addEventListener("click", ()=>{
+    window.location.href = "https://www.beautyhandsvr.com";
+})
+
+web3.addEventListener("click", ()=>{
+    window.location.href = "https://www.psicologamariaadriana.com";
+})
