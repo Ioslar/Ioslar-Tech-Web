fcfdf078ffef3bb34ba4105d0f2e2e1170b4e417
Ioslar <101954529+Ioslar@users.noreply.github.com>
2023-09-07 12:45:07 -0300

    Update portfolio-script.js

--- Portfolio Page/portfolio-script.js
+++ Portfolio Page/portfolio-script.js
@@ -5,13 +5,5 @@ const web2 = document.querySelector(".web-2");
 const web3 = document.querySelector(".web-3");
 
 web1.addEventListener("click", ()=>{
-    window.location.href = "https://www.academiashopping.com.uy";
-})
-
-web2.addEventListener("click", ()=>{
-    window.location.href = "https://www.beautyhandsvr.com";
-})
-
-web3.addEventListener("click", ()=>{
-    window.location.href = "https://www.psicologamariaadriana.com";
+    window.location.href = "https://ioslar.github.io/Academia-Shopping-Web/";
 })
